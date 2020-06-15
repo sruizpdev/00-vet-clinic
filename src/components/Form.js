8c422854b2c21274d1d38b6c3f6e5609f956ca82
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-06-15 13:02:40 +0200

    validating form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,51 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 const Form = () => {
+  const [appointment, updateAppointment] = useState({
+    pet: '',
+    owner: '',
+    date: '',
+    time: '',
+    description: '',
+  });
+  const [error, updateError] = useState(false);
+
+  const updateState = (e) => {
+    e.preventDefault();
+    updateAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { pet, owner, date, time, description } = appointment;
+
+  const submitAppointment = (e) => {
+    e.preventDefault();
+    if (
+      pet.trim() === '' ||
+      owner.trim() === '' ||
+      date.trim() === '' ||
+      time.trim() === '' ||
+      description.trim() === ''
+    ) {
+      updateError(true);
+
+      return;
+    }
+  };
   return (
     <Fragment>
       <h1>New appoinment</h1>
-      <form>
+      {error ? <p className="alert-error">An error has occurred</p> : null}
+      <form onSubmit={submitAppointment}>
         <label>Pet name</label>
         <input
           type="text"
           name="pet"
           className="u-full-width"
           placeholder="Pet name"
+          onChange={updateState}
+          value={pet}
         />
         <label>Owner name</label>
         <input
@@ -17,13 +53,32 @@ const Form = () => {
           name="owner"
           className="u-full-width"
           placeholder="Owner name"
+          onChange={updateState}
+          value={owner}
         />
         <label>Date</label>
-        <input type="date" name="date" className="u-full-width" />
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={updateState}
+          value={date}
+        />
         <label>Time</label>
-        <input type="time" name="time" className="u-full-width" />
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+          onChange={updateState}
+          value={time}
+        />
         <label>Description</label>
-        <textarea name="description" className="u-full-width"></textarea>
+        <textarea
+          name="description"
+          className="u-full-width"
+          onChange={updateState}
+          value={description}
+        ></textarea>
         <button type="submit" className="u-full-width button-primary">
           New appoinment
         </button>
